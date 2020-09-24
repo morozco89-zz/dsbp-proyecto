9e69058778ed7c807cefee12c602afd97e8abaed
Miguel Cortés <ing.m.cortes@gmail.com>
2020-09-23 19:28:52 -0500

    First commit

---
+++ src/test/java/co/edu/javeriana/dsbp/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.dsbp.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
